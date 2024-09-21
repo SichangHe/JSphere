42dd7a162c3064b03b964c194c942fc879e551d2
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-09-20 22:19:34 -0700

    chore: clean up container after exit

--- headless_browser/run.sh
+++ headless_browser/run.sh
@@ -15,10 +15,12 @@ chmod 777 target
 # with namespaces support on,
 # mounting current directory at `run/`,
 # with `entrypoint.sh` as the entry point,
+# removing the container after it exits,
 # the VV8 image.
 docker run \
     --user 0 \
     --cap-add=SYS_ADMIN \
     -v "$(pwd):/home/node/run" \
     --entrypoint "run/entrypoint.sh" \
+    --rm \
     $IMAGE_NAME
