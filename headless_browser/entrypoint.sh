a18b259456d4ccb926064aff2ed7855da0760071
Steven Hé (Sīchàng) <stevensichanghe@gmail.com>
2024-09-29 14:55:19 -0700

    chore: directly run server w/ docker command fix: pnpm do not update lockfile

--- headless_browser/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-set -e
-
-SCRIPT_DIR="$(dirname "$0")"
-cd "$SCRIPT_DIR"
-node server.js
--- headless_browser/run.sh
+++ headless_browser/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 if test ! -d "node_modules"; then
-    pnpm i
+    pnpm i --frozen-lockfile
 fi
 
 IMAGE_NAME="visiblev8/vv8-base"
@@ -17,10 +17,13 @@ chmod 777 target
 # with `entrypoint.sh` as the entry point,
 # removing the container after it exits,
 # the VV8 image.
+# shellcheck disable=SC2068
 docker run \
     --user 0 \
     --cap-add=SYS_ADMIN \
     -v "$(pwd):/home/node/run" \
-    --entrypoint "run/entrypoint.sh" \
+    --entrypoint "/usr/local/bin/node" \
+    --workdir "/home/node/run" \
     --rm \
-    $IMAGE_NAME
+    $IMAGE_NAME \
+    server.js $@
